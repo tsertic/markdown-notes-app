e87aa133bc31647f7b09a8345c299acafbb382d8
Tomislav Sertic <tsertic5@gmail.com>
2025-08-12 08:36:37 +0200

    Dashboard - Fetching and Displaying Notes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from "../config/firebase";
 const Navbar = () => {
+  const { currentUser } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/login");
+    } catch (error) {
+      console.error("Something went wrong with logout", error.message);
+    }
+  };
   return (
     <nav className="bg-white shadow-md dark:bg-gray-800">
       <div className="container mx-auto px-4">
@@ -13,18 +26,34 @@ const Navbar = () => {
             Markdown Notes
           </Link>
           <div className="flex items-center space-x-4">
-            <Link
-              to="/login"
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-            >
-              Login
-            </Link>
-            <Link
-              to="/register"
-              className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white"
-            >
-              Register
-            </Link>
+            {currentUser ? (
+              <>
+                <span className="text-gray-700 dark:text-gray-300 hidden text-sm sm:block">
+                  {currentUser.email}
+                </span>
+                <button
+                  onClick={handleLogOut}
+                  className="bg-blue-500 hover:bg-blue-600 rounded block font-bold px-3 py-2 cursor-pointer text-white"
+                >
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link
+                  to="/login"
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                >
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white"
+                >
+                  Register
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,105 @@
 import { getAuth, signOut } from "firebase/auth";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { db } from "../config/firebase";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   const auth = getAuth();
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
   const { currentUser } = useAuth();
-  console.log(currentUser);
-  const handleLogOut = () => {
-    signOut(auth)
-      .then(() => {
-        console.log("User sign out");
-      })
-      .catch((err) => {
-        console.error(err.message);
-      });
-  };
+
+  useEffect(() => {
+    if (!currentUser) {
+      setLoading(false);
+      return;
+    }
+    const notesCollectionRef = collection(
+      db,
+      "users",
+      currentUser.uid,
+      "notes"
+    );
+
+    const q = query(notesCollectionRef, orderBy("updatedAt", "desc"));
+
+    const unsubscribe = onSnapshot(
+      q,
+      (querySnapshot) => {
+        const notesData = [];
+        querySnapshot.forEach((doc) => {
+          notesData.push({ id: doc.id, ...doc.data() });
+        });
+        setNotes(notesData);
+        setLoading(false);
+      },
+      (err) => {
+        console.error(err);
+        setError("Failed to fetch notes.");
+        setLoading(false);
+      }
+    );
+
+    return () => unsubscribe();
+  }, [currentUser]);
+
+  if (loading) {
+    return <div className="text-center mt-10 ">Loading your notes..</div>;
+  }
+  if (error) {
+    return <div className="text-center text-red-500 mt-10">{error}</div>;
+  }
+
   return (
     <div>
-      <div className="p-2 border rounded"> hello {currentUser.displayName}</div>
-      <button className="m-2 block bg-blue-400" onClick={handleLogOut}>
-        log out
-      </button>
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold">Your Notes</h1>
+        <Link
+          to="/note/new"
+          className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-3 rounded cursor-pointer"
+        >
+          {" "}
+          + New Note
+        </Link>
+      </div>
+      {notes.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {notes.map((note) => {
+            <div
+              key={note.id}
+              className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow hover:shadow-lg transition-shadow"
+            >
+              <h2 className="text-2xl font-semibold truncate mb-2">
+                {note.title}
+              </h2>
+              <p className="text-gray-400 dark:text-gray-500 text-sm h-12 overflow-hidden">
+                {note.content.substring(0, 100)}
+              </p>
+              <div className="text-gray-400 dark:text-gray-500 text-xs mt-4">
+                Last updated :{" "}
+                {new Date(note.updatedAt?.toDate()).toLocaleString()}
+              </div>
+            </div>;
+          })}
+        </div>
+      ) : (
+        <div className="text-center p-8 mt-10 bg-white dark:bg-gray-800 rounded-lg shadow">
+          <h2 className="text-xl font-semibold">No notes yet!</h2>
+          <p className="mt-2 text-gray-600 dark:text-gray-400">
+            {" "}
+            Click the 'New Note' button to get started.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
